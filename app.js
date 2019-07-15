333ecd583613e38715f2751d852b734cb04bbbc0
Duhoon <eclip6@naver.com>
2019-07-15 15:53:08 +0900

    stylesheet add

--- app.js
+++ app.js
@@ -2,6 +2,46 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+
+const path = require('path');
+const router = express.Router();
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/stylesheet01', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/stylesheet01.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/stylesheet02', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/stylesheet02.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/stylesheet03', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/stylesheet03.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/stylesheet04', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/stylesheet04.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/nostylesheet', function (req, res) {
+    res.sendFile(path.join(__dirname + '/views/nostylesheet.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+app.use('/', router);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
